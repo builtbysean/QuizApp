637b17ad39ce876cba41bcdd483f0e1dbd050547
Sean Coffin <seancoffin2003@gmail.com>
2025-11-09 15:09:40 -0500

    Initial Commit

---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Sean Coffin on 11/9/25.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
